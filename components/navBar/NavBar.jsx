568b4e4fa56abfbb397ce67113887f07768a38b3
Jay-Sparks <jay_sparks@icloud.com>
2024-03-11 18:54:44 +0000

    planning artico front-end

---
+++ components/navBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import styles from './NavBar.module.css'
+
+function NavBar() {
+  return (
+    <nav>
+        <p>Home nav</p>
+        <p>Explore nav</p>
+        <p>Publish nav</p>
+        <p>Account nav</p>
+      </nav>
+  )
+}
+
+export default NavBar
